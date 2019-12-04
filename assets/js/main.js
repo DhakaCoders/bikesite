b94015b6851d16889aa6294f651af3740e8072f0
Rannojit <cserannojitsarkar22@gmail.com>
2019-12-05 02:44:45 +0600

    initial home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,21 @@ if( $('.main-bnr-slider').length ){
     });
 }
 
+if( $('#slider-range').length ){
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 50000,
+      max: 700000,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "BDT " + ui.values[ 0 ] + " - BDT " + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "BDT " + $( "#slider-range" ).slider( "values", 0 ) +
+      " - BDT " + $( "#slider-range" ).slider( "values", 1 ) );
+}
+
+
 /**
 Slick slider
 */
