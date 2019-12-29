ce88c9576cc1764d15dd44ba399363f7c44a45a3
Rannojit <cserannojitsarkar22@gmail.com>
2019-12-30 02:05:25 +0600

    compare single page improve

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 (function($) {
+if($('#preloader').length){
+  $('#status1').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(550).css({'overflow':'visible'});
+
+};
 
 $('.navbar-toggle').on('click', function(){
 	$('#mobile-nav').slideToggle(300);
