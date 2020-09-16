1dcdaeb5c87cd3d09e1e9db11878ef448fe8eb3e
azizulsoftsw <azizul.softsw@gmail.com>
2020-09-16 12:05:28 +0600

    Update search.php

--- themes/bikesite/search.php
+++ themes/bikesite/search.php
@@ -60,7 +60,7 @@ get_header();
                       <?php }else{ ?>
                         <div class="product-content-wrapper">
                           <div class="product-title-spreed">
-                              <?php the_title(); ?>
+                              <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
                           </div>
                         </div>
                       <?php } ?>
